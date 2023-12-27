808e3aa9dd79cfa0143046ec0dc7bf04bdb5d0c8
138636784+thinhphan1122@users.noreply.github.com <thinhphanvn8499@gmail.com>
2023-12-27 15:28:02 +0700

    Create the following entities: Contract and ContractDetails

--- src/main/java/com/codegym/bestticket/entity/contract/ContractDetail.java
+++ src/main/java/com/codegym/bestticket/entity/contract/ContractDetail.java
@@ -1,4 +1,41 @@
 package com.codegym.bestticket.entity.contract;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "contract_details")
 public class ContractDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "contract_id", nullable = false)
+    private Contract contract;
+
+    @Column(name = "ticket_id")
+    private UUID ticketId;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "ticket_price")
+    private Double ticketPrice;
 }
