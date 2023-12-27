d85c16b8ae59feb5351cdcbc8ce7edaefd51e2d7
PhaiP <pphp1998@gmail.com>
2023-12-27 16:08:06 +0700

    create entity event,eventype,time

--- src/main/java/com/codegym/bestticket/entity/event/Time.java
+++ src/main/java/com/codegym/bestticket/entity/event/Time.java
@@ -1,4 +1,28 @@
 package com.codegym.bestticket.entity.event;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Table(name = "TIME")
 public class Time {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "time")
+    private LocalDateTime time;
+
+    @ManyToMany(mappedBy = "times")
+    private Set<Event> events;
+
 }
