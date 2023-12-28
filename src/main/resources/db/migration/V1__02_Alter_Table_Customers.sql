e703f191c8572b151ea932d01b279192919fc9c1
NhanTran262 <tranhoangnhan262@gmail.com>
2023-12-28 22:20:06 +0700

    update

--- src/main/resources/db/migration/V1__02_Alter_Table_Customers.sql
+++ src/main/resources/db/migration/V1__02_Alter_Table_Customers.sql
@@ -1,5 +1,5 @@
-RENAME TABLE customer TO customers;
-ALTER TABLE customers
-    ADD COLUMN user_customer_id binary(36),
-    ADD CONSTRAINT  user_customer_id FOREIGN KEY (user_customer_id) REFERENCES users (id);
-
+# RENAME TABLE customer TO customers;
+# ALTER TABLE customers
+#     ADD COLUMN user_customer_id binary(36),
+#     ADD CONSTRAINT  user_customer_id FOREIGN KEY (user_customer_id) REFERENCES users (id);
+#
--- src/main/resources/db/migration/V1__05_Drop_Role_Id.sql
+++ src/main/resources/db/migration/V1__05_Drop_Role_Id.sql
@@ -1,3 +1,3 @@
-ALTER TABLE users
-DROP FOREIGN KEY role_id,
-    DROP COLUMN role_id;
+# ALTER TABLE users
+# DROP FOREIGN KEY role_id,
+#     DROP COLUMN role_id;
