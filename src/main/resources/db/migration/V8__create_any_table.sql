17deca3e474e9ede3eb8c2b8bd4e1e582febea16
NhanTran262 <tranhoangnhan262@gmail.com>
2023-12-28 09:58:45 +0700

    Update V8__create_any_table

---
+++ src/main/resources/db/migration/V8__create_any_table.sql
@@ -0,0 +1,32 @@
+CREATE TABLE event_types
+(
+    id   BINARY(36) PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE events
+(
+    id            BINARY(36) PRIMARY KEY,
+    name          VARCHAR(50)  NOT NULL,
+    address       VARCHAR(255) NOT NULL,
+    event_type_id BINARY(36),
+    FOREIGN KEY (event_type_id) REFERENCES event_types (id)
+);
+
+CREATE TABLE times
+(
+    id   BINARY(36) PRIMARY KEY ,
+    time datetime
+);
+
+CREATE TABLE event_times
+(
+    event_id BINARY(36),
+    time_id  BINARY(36),
+    PRIMARY KEY (event_id, time_id),
+    FOREIGN KEY (event_id) REFERENCES events (id),
+    FOREIGN KEY (time_id) REFERENCES times (id)
+);
+
+
+
