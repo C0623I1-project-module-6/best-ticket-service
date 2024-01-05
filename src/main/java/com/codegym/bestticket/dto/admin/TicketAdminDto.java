e66babbb9351376f3cdcacd829bfbae542f08a3f
AN-HA\AN HA <baoandlk93@gmail.com>
2024-01-05 15:59:40 +0700

    update code in admin function

---
+++ src/main/java/com/codegym/bestticket/dto/admin/TicketAdminDto.java
@@ -0,0 +1,11 @@
+package com.codegym.bestticket.dto.admin;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class TicketAdminDto {
+    private UUID ticketId;
+
+}
--- src/main/java/com/codegym/bestticket/repository/booking/IBookingRepository.java
+++ src/main/java/com/codegym/bestticket/repository/booking/IBookingRepository.java
@@ -1,6 +1,7 @@
 package com.codegym.bestticket.repository.booking;
 
 import com.codegym.bestticket.entity.booking.Booking;
+import com.codegym.bestticket.entity.ticket.Ticket;
 import com.codegym.bestticket.entity.user.Customer;
 import com.codegym.bestticket.entity.user.Organizer;
 import org.springframework.data.domain.Page;
@@ -17,4 +18,5 @@ public interface IBookingRepository extends JpaRepository<Booking, UUID> {
 
     Page<Booking> getBookingsByCustomer_Id(Pageable pageable, UUID uuid);
     Page<Booking> findAllByIsDeletedFalse(Pageable pageable);
+
 }
