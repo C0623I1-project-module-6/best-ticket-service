9483aa9ed58d152f035c48a22fd106735b22a054
NhanTran262 <tranhoangnhan262@gmail.com>
2024-01-28 00:06:12 +0700

    Update forgot password

---
+++ src/main/java/com/codegym/bestticket/payload/request/VerifyOtpRequest.java
@@ -0,0 +1,10 @@
+package com.codegym.bestticket.payload.request;
+
+import lombok.Data;
+
+@Data
+public class VerifyOtpRequest {
+    private String email;
+    private String otp;
+    private String newPassword;
+}
