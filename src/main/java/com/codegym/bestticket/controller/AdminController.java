33bad868323ab9ad2af2ee5e459537292ed04753
AN-HA\AN HA <baoandlk93@gmail.com>
2024-02-21 09:14:43 +0700

    update 21/02/2024

--- src/main/java/com/codegym/bestticket/controller/AdminController.java
+++ src/main/java/com/codegym/bestticket/controller/AdminController.java
@@ -1,31 +1,16 @@
 package com.codegym.bestticket.controller;
 
-import com.codegym.bestticket.dto.user.OrganizerTypeDto;
-import com.codegym.bestticket.dto.user.RoleDto;
-import com.codegym.bestticket.entity.booking.Booking;
-import com.codegym.bestticket.entity.ticket.Ticket;
 import com.codegym.bestticket.payload.ResponsePayload;
 import com.codegym.bestticket.service.IAdminService;
-import com.codegym.bestticket.service.IOrganizerTypeService;
-import com.codegym.bestticket.service.IRoleService;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -36,8 +21,13 @@ public class AdminController {
 
 
     @GetMapping("/users")
-    public ResponseEntity<ResponsePayload> shows(@PageableDefault(size = 8) Pageable pageable) {
+    public ResponseEntity<ResponsePayload> showUsers(@PageableDefault(size = 8) Pageable pageable) {
         ResponsePayload responsePayload = adminService.showUsers(pageable);
+        return new ResponseEntity<>(responsePayload, responsePayload.getStatus());
+    }
+    @GetMapping("/events")
+    public ResponseEntity<ResponsePayload> showEvents(@PageableDefault(size = 8) Pageable pageable) {
+        ResponsePayload responsePayload = adminService.showEvents(pageable);
         return new ResponseEntity<>(responsePayload, responsePayload.getStatus());
     }
 
