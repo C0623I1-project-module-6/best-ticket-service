8122f6eaede684f72f4d231bb8a9500d3c7aafce
NhanTran262 <tranhoangnhan262@gmail.com>
2024-03-07 10:58:18 +0700

    Update done upload image with firebase storage

--- src/main/java/com/codegym/bestticket/controller/FileEntityController.java
+++ src/main/java/com/codegym/bestticket/controller/FileEntityController.java
@@ -2,11 +2,15 @@ package com.codegym.bestticket.controller;
 
 import com.codegym.bestticket.payload.ResponsePayload;
 import com.codegym.bestticket.service.IFileEntityService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,6 +28,9 @@ public class FileEntityController {
 
     @PostMapping("/upload")
     public ResponseEntity<ResponsePayload> uploadFile(MultipartFile file) {
+        if (file == null) {
+            new ResponseEntity<>("File not found!", HttpStatus.NOT_FOUND);
+        }
         ResponsePayload responsePayload = fileEntityService.save(file);
         return new ResponseEntity<>(responsePayload, responsePayload.getStatus());
     }
--- src/main/java/com/codegym/bestticket/service/IFileEntityService.java
+++ src/main/java/com/codegym/bestticket/service/IFileEntityService.java
@@ -1,12 +1,15 @@
 package com.codegym.bestticket.service;
 
 import com.codegym.bestticket.payload.ResponsePayload;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 public interface IFileEntityService {
     ResponsePayload findAll();
 
     ResponsePayload save(MultipartFile file);
 
-    ResponsePayload findById(Long id);
+    ResponsePayload findById(UUID id);
 }
